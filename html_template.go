7f39e0288c3664da0806fbad6839d076e93ce2db
Parker Moore <parkrmoore@gmail.com>
2020-08-08 12:17:40 -0400

    Add HTML for submitting

--- html_template.go
+++ html_template.go
@@ -1,8 +1,92 @@
 package untrackthaturl
 
-import "io"
+import (
+	"html/template"
+	"io"
+)
+
+var indexTemplate = template.Must(template.New("index.html").Parse(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta content="origin-when-cross-origin" name="referrer" />
+  <title>Untrack That URL</title>
+  <style type="text/css">
+  .markdown-body {
+      width: 95%;
+	  margin: 0 auto;
+	  text-align: center;
+  }
+  #errors { color: red; }
+  </style>
+  <script type="application/javascript">
+    function writeError(error) {
+		var errorDiv = document.getElementById("errors");
+		errorDiv.innerText = "error: "+error;
+	}
+    function reqListener () {
+      console.log(this.responseText);
+
+      if (this.responseText === null || this.responseText === "") {
+        console.error("nada");
+        return;
+      }
+
+	  var info = JSON.parse(this.responseText);
+
+	  if (info.Error !== undefined) {
+		writeError(info.Error);
+		return
+	  }
+
+      var resultDiv = document.getElementById("resultURL");
+      resultDiv.innerText = "";
+
+      var anchor = document.createElement("a");
+      anchor.href = info.URL;
+      anchor.title = "Resolved URL";
+      anchor.innerText = info.URL;
+	  resultDiv.appendChild(anchor);
+    }
+
+	function submitResolveURL() {
+	  document.getElementById("errors").innerText = ""
+
+	  var inputURL = document.getElementById("url").value;
+      if (inputURL === undefined || inputURL === "") {
+		  writeError("input URL required");
+		  return
+	  }
+	  var params = 'url='+escape(inputURL);
+
+	  var oReq = new XMLHttpRequest();
+  	  oReq.addEventListener("load", reqListener);
+	  oReq.open("POST", "/resolve.json", true);
+	  oReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	  oReq.send(params);
+	}
+
+    </script>
+  </head>
+  <body>
+    <div class="markdown-body">
+	  <p>Enter a URL to resolve:</p>
+	  <p>
+	    <div id="errors"></div>
+        <label for="name">URL:</label>
+	    <input type="text" id="url" name="url" required>
+		<input type="button" onclick="submitResolveURL();" value="Resolve">
+	  </p>
+	  <p>
+		<div id="resultURL"></div>
+	  </p>
+    </div>
+  </body>
+</html>
+`))
 
 func RenderHTML(w io.Writer) error {
-	_, err := w.Write([]byte(`not implemented yet`))
-	return err
+	return indexTemplate.Execute(w, nil)
 }
--- resolver.go
+++ resolver.go
@@ -4,26 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-
-	"mvdan.cc/xurls/v2"
 )
 
-func fetchRedirectURL(resp *http.Response) string {
-	location := resp.Header.Get("Location")
-	if location != "" {
-		return location
-	}
-	link := resp.Header.Get("Link")
-	if link != "" {
-		urls := xurls.Relaxed().FindAllString(link, -1)
-		if len(urls) > 0 {
-			return urls[0]
-		}
-	}
-
-	return ""
-}
-
+// ResolveURL takes an input URL, sends a HEAD request to it and follows all HTTP redirects, returning the resulting URL.
 func ResolveURL(u *url.URL) (*url.URL, error) {
 	resp, err := http.Head(u.String())
 	if err != nil {
